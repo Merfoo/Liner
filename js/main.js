4eb3db3dfbf9d0b0b80a6770c3fbd825e7374c7e
Merfoo <fauzimerfoo@gmail.com>
2014-05-01 17:17:43 -0700

    Added breaks, changed line caps, added touch area, added line direction, added comments to functions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 // Gobal variables, starting with _
 var _cvs = { game: null };
 var _map = { width: 0, height: 0 };
-var _line = { body: [], width: 10 };
+var _touchArea = { y: 300, height: 200, color: "white" };
+var _line = { body: [], normWidth: 15, smallWidth: 10, bigWidth: 20 };
 var _mouse = { x: null, y: null, down: false };
+var _lineMode = { left: false, center: false, right: false, none: false, count: 0, minCount: 0, maxCount: 25 };
 
 document.addEventListener("DOMContentLoaded", initGame);
 document.documentElement.style.overflowX = "hidden";	 // Horizontal scrollbar will be hidden
@@ -20,8 +22,16 @@ function initGame()
     _map.height = window.innerHeight - (cvsBorderThick * 2);
     _cvs.game.canvas.width = _map.width;
     _cvs.game.canvas.height = _map.height;
-    setInterval(gameLoop, 1000 / 60);
-    _line.body.push({ x: 0, y: 0 });
+    _line.body.push({ x: _map.width / 2, y: 0, visible: true });
+    
+    window.requestAnimFrame = window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+        };
+        
+    gameLoop();
 }
 
 // Main loop for the game
@@ -29,50 +39,108 @@ function gameLoop()
 {
     clearScreen();
     updateLine();
+    paintTouchArea();
     paintLine();
+    window.requestAnimFrame(gameLoop);
 }
 
 // Adds new points to the line
 function updateLine()
 {   
-    var x, y;
+    var lastPos = { x: _line.body[0].x, y: _line.body[0].y };
     
-    if(_mouse.x !== null && _mouse.y !== null)
+    if(lastPos.x < 0 + _line.width)
     {
-        x = getRandomNumber(_mouse.x - 200, _mouse.x + 200);
-        y = getRandomNumber(_mouse.y - 200, _mouse.y + 200);
+        resetLineMode();
+        _lineMode.right = true;
     }
     
-    else
+    if(lastPos.x > _map.width - _line.width)
     {
-        x = getRandomNumber(0, _map.width);
-        y = getRandomNumber(0, _map.height);
+        resetLineMode();
+        _lineMode.left = true;
     }
     
-    _line.body.unshift({ x: x, y: y });
+    if(--_lineMode.count <= 0)
+    {
+        resetLineMode();
+        _lineMode.count = getRandomNumber(_lineMode.minCount, _lineMode.maxCount);
+        var mode = getRandomNumber(1, 1000);
+        
+        if(mode < 250)
+            _lineMode.left = true;
+        
+        else if(mode < 500)
+            _lineMode.center = true;
+        
+        else if(mode < 750)
+            _lineMode.right = true;
+        
+        else
+            _lineMode.none = true;
+    }
+    
+    var dist = getRandomNumber(1, 5);
+    
+    if(_lineMode.left)
+        dist *= -1;
+    
+    else if(_lineMode.center)
+        dist *= 0;
+    
+    else if(_lineMode.right)
+        dist *= 1;
+    
+    var p = { x: lastPos.x + dist, y: 0, visible: true, width: _line.normWidth };
+    p.visible = !_lineMode.none;
+    _line.body.unshift(p);
  
     for(var i in _line.body)
     {
         _line.body[i].y += 10;
         
-        if(_line.body[i].y > _map.height + (_map.height / 3))
+        if(_line.body[i].y > _map.heigh + (_map.height / 3))
            _line.body.splice(i, 1);
     }
+}
+
+// Resets the the lineMode
+function resetLineMode()
+{
+    _lineMode.left = false;
+    _lineMode.center = false;
+    _lineMode.right = false;
+    _lineMode.none = false;
 }
 
 // Paints the line body
 function paintLine()
 {
     _cvs.game.beginPath();
-    _cvs.game.lineWidth = _line.width;
+    _cvs.game.lineCap = "round";
     _cvs.game.moveTo(_line.body[0].x, _line.body[0].y);
     
     for(var i = 1; i < _line.body.length; i++)
-        _cvs.game.lineTo(_line.body[i].x, _line.body[i].y);
+    {    
+        _cvs.game.lineWidth = _line.body[i].width;
+        
+        if(_line.body[i].visible)
+            _cvs.game.lineTo(_line.body[i].x, _line.body[i].y);
+        
+        else
+            _cvs.game.moveTo(_line.body[i].x, _line.body[i].y);
+    }
     
     _cvs.game.strokeStyle = getRandomColor(0, 255);
     _cvs.game.stroke();
     _cvs.game.closePath();
+}
+
+// Paints the touch area
+function paintTouchArea()
+{
+    _cvs.game.fillStyle = _touchArea.color;
+    _cvs.game.fillRect(0, _touchArea.y, _map.width, _touchArea.height);
 }
 
 // Paints the whole screen
